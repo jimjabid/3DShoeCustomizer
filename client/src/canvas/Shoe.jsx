652a264fe9e5f9c74e8a12c10925b0960a7dc190
jimjabid <jabidandresjimenezserrano@gmail.com>
2023-08-15 01:10:01 -0300

    Adding Color on Change to Backdrop

--- client/src/canvas/Shoe.jsx
+++ client/src/canvas/Shoe.jsx
@@ -3,6 +3,7 @@ import { useSnapshot } from 'valtio'
 import { useFrame } from '@react-three/fiber'
 import {Decal,useGLTF, useTexture} from "@react-three/drei"
 import state from "../store"
+import { easing } from 'maath'
 
 function Shoe(props)  {
     const snap = useSnapshot(state);
@@ -23,6 +24,10 @@ function Shoe(props)  {
       }
       
     }, [hovered])
+
+    // useFrame((state, delta) => easing.dampC(materials.laces.color, snap.items[snap.current], 0.25, delta));
+
+     
 
   return (
     <group  {...props} dispose={null}
